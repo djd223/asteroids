d11297a058cd508b55781f7368e84392b897065d
djd223 <davissond99@gmail.com>
2024-08-21 17:56:00 -0700

    finished movement for player class and added group containers

--- main.py
+++ main.py
@@ -6,16 +6,28 @@ def main():
   pygame.init
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
-  dt = 0
+
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  Player.containers = (updatable, drawable)
 
   player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+  dt = 0
 
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+
+    for each in updatable:
+      each.update(dt)
+
     screen.fill("black")
-    player.draw(screen)
+
+    for each in drawable:
+      each.draw(screen)
+ 
     pygame.display.flip()
 
     dt = (clock.tick(60))/1000
